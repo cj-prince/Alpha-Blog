cac4724811f71c9016a30711290c6fe58b5254e9
Davids Chijioke Prince <davids.chijioke@gmail.com>
2021-04-06 21:34:22 -0400

    Create show action and gravtar image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def gravtar_for(user, options = {size:80})
+        gravtar_id = Digest::MD5.hexdigest(user.email.downcase)
+        size = options[:size]
+        gravtar_url = "https://www.gravatar.com/avatar/#{gravtar_id}?s=#{size}"
+        image_tag(gravtar_url, alt:user.username, class: "img-circle")
+    end
 end
